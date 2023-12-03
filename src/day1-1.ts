72cd2584702c2abe4177019839c0895057221fc2
Laurie Trichet <laurie@twobulls.com>
2023-12-03 22:57:44 +1100

    complete part 1 day 1

--- src/day1-1.ts
+++ src/day1-1.ts
@@ -3,7 +3,8 @@ import { reduceTransformLinesFromInputFile } from "./getLinesFromFile";
 import { accumulativeAddition } from "./mathHelpers";
 
 async function execute() {
-    const test = await reduceTransformLinesFromInputFile("./assets/input-test.txt", combineFirstAndLastDigit, accumulativeAddition);
+    // const test = await reduceTransformLinesFromInputFile("./assets/input-test.txt", combineFirstAndLastDigit, accumulativeAddition);
+    const test = await reduceTransformLinesFromInputFile("./assets/input.txt", combineFirstAndLastDigit, accumulativeAddition);
     console.log(test);
 }
 
