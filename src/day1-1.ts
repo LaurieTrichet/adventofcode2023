362105b2b289470faba4cfb3b6682a2d0f1b049c
Laurie Trichet <laurie@twobulls.com>
2023-12-03 22:28:45 +1100

    test are broken, cannot resolve node path.

--- src/day1-1.ts
+++ src/day1-1.ts
@@ -1,8 +1,10 @@
+import { open } from "node:fs/promises";
+import path from "node:path";
+
 export function combineFirstAndLastDigit(input: string) {
     const firstDigit = lookForDigit(input);
     const reverseInput = [...input].reverse();
     const lastDigit = lookForDigit(reverseInput.join());
-
     return (firstDigit && lastDigit) ? parseInt(firstDigit.concat(lastDigit)) : undefined;
 }
 
@@ -17,5 +19,19 @@ function lookForDigit(input: string) {
     return undefined;
 }
 
+export async function reduceTransformLinesFromInputFile(inputFilePath: string,
+    transform: (input: string) => (number | undefined),
+    reduceFunction: (previousValue: number | undefined, currentValue: number | undefined, currentIndex: number, array: (number | undefined)[]) => number | undefined) {
+    const filePath = path.join(path.resolve("."), inputFilePath);
+    const file = await open(filePath);
+    const result: (number | undefined)[] = [];
+    for await (const line of file.readLines()) {
+        console.log(line);
+        result.push(transform(line));
+    }
+    return result.reduce(reduceFunction);
+}
 
-
+export function add(previousValue: number | undefined, currentValue: number | undefined) {
+    return (previousValue ?? 0) + (currentValue ?? 0);
+}
--- tests/day1-1.test.ts
+++ tests/day1-1.test.ts
@@ -1,8 +1,12 @@
-import { combineFirstAndLastDigit } from "../src/day1-1";
+import { add, combineFirstAndLastDigit, reduceTransformLinesFromInputFile, } from "../src/day1-1";
 
 test('combineFirstAndLastDigit ', () => {
     expect(combineFirstAndLastDigit("1abc2")).toBe(12);
     expect(combineFirstAndLastDigit("pqr3stu8vwx")).toBe(38);
     expect(combineFirstAndLastDigit("a1b2c3d4e5f")).toBe(15);
     expect(combineFirstAndLastDigit("treb7uchet")).toBe(77);
+});
+
+test("sumAllCalibrationsInDocument", () => {
+    expect(reduceTransformLinesFromInputFile("../asset/input-test.txt", combineFirstAndLastDigit, add)).toBe(142);
 });
